3e86d9290497d31f08879ee937545f71f0e7e4a6
hokjanbaruda <hokjanbaruda@gmail.com>
2025-07-14 22:00:49 +0700

    Delete src directory

--- src/utils/site.ts
+++
@@ -1,4 +0,0 @@
-export const nama = 'Montok Semok';
-export const desk = 'Video Bokep Indo Jepang Jav Hd Artis Viral Terbaru Bocil Ngentot Jilbab Smp Montok';
-export const url = 'https://montoksemok.pages.dev';
-export const terbit = '2025-06-26T00:01:03Z';
